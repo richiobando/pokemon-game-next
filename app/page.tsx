dd4e0adcf55bac07c09ae6b3198a94379d2435c7
Ricardo <ricardobvil116@gmail.com>
2022-12-14 22:50:23 -0600

    (feat) types Cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,20 @@
 import Image from 'next/image'
 import styles from './page.module.css'
-
+import  {Cards} from '../types'
 export default async function Home() {
   const { data } = await import('../data/cards.json');
-  const cards = data.slice(0,9)
+  const cards:Cards[] = data.slice(0,9)
   return (
     <main>
       <div className='grid grid-cols-3 gap-4'>
-        {cards.map(card =>(
-          <div>
+        {cards.map((card,i) =>(
+          <div key={i}>
             <div>{card.name}</div>
             <img src={card.images.small} alt={card.name} />
-            <p>Normal: ${card?.tcgplayer.prices.normal?.mid}</p>
-            <p>Holofoil: ${card?.tcgplayer.prices.holofoil?.mid}</p>
-            <p>Holofoil: ${card?.tcgplayer.prices.holofoil?.mid}</p>
+            <p>Holofoil: ${card?.tcgplayer?.prices?.holofoil?.mid}</p>
           </div>
         ))}
       </div>
-   </main>
+    </main>
   )
 }
